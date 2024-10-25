e9b6b0c4fde3e3d41eac99830babe59f3d4cbbdd
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-10-25 13:37:45 +0200

    portfolio principal

---
+++ portfolio-main/js/script.js
@@ -0,0 +1,62 @@
+window.addEventListener("scroll", function () {
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo", window.scrollY > 0);
+})
+
+var nav = document.getElementById("nav");
+var abrir = document.getElementById("abrir");
+var cerrar = document.getElementById("cerrar");
+
+abrir.addEventListener("click", function () {
+    nav.classList.add("visible");
+})
+
+
+cerrar.addEventListener("click", function () {
+    nav.classList.remove("visible");
+})
+
+
+var emailForm = document.getElementById("email");
+var subjectForm = document.getElementById("subject");
+var messageForm = document.getElementById("message");
+var form = document.getElementById("contact-form");
+//Para vaciar los parametros y lanzar el mensaje
+form.addEventListener("submit", function (event) {
+    if (emailForm.value && subjectForm.value && messageForm.value) {
+        //Hacemos que se haga un poco mas tarde para que de tiempo a que el submit se haga
+        setTimeout(function () {
+            emailForm.value = '';
+            subjectForm.value = '';
+            messageForm.value = '';
+            Swal.fire({
+                title: "Correo enviado!",
+                text: "Le responderé lo antes posible!",
+                icon: "success",
+                customClass: {
+                    confirmButton: 'my-custom-button'
+                }
+            });
+        }, 100)
+    } else {
+        event.preventDefault();
+        Swal.fire({
+            title: "Rellene todos campos",
+            icon: "error",
+            customClass: {
+                confirmButton: 'my-custom-button'
+            }
+        });
+    }
+});
+
+
+document.querySelector('.link-numero').addEventListener('click', () => {
+    Swal.fire({
+        text: "+34 685 82 76 50",
+        icon: "info",
+        customClass: {
+            confirmButton: 'my-custom-button'
+        }
+    });
+})
